5cd4ed1a77a6740be4ece7a8599cc48bca3cc172
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-23 13:41:35 -0800

    event class constructor render event method

--- frontend/src/event.js
+++ frontend/src/event.js
@@ -1,1 +1,22 @@
-class Event
+class Event {
+
+    constructor(event) {
+        this.id = event.id
+        this.name = event.attributes.name
+        this.description = event.attributes.description
+        this.participants = event.attributes.participants
+        Event.all.push(this)
+    }
+
+    renderEvent() {
+        const eventList = document.querySelector('#event-list')
+        const eventListMarkup = `${this.name}`
+        let eventListOption = document.createElement('option')
+        eventListOption.innerHTML = eventListMarkup
+        eventListOption.setAttribute('id', this.id)
+        eventListOption.setAttribute('value', this.id)
+        eventList.appendChild(eventListOption)
+    }
+}
+
+Event.all = []
